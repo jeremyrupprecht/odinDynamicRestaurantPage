b7c1cc7ab3adf6f61713e8a557ecacdf7193f52b
Jeremy <jdavid.rupprecht@gmail.com>
2023-12-08 01:46:55 -0700

    Add JS to generate the home page

--- src/home.js
+++ src/home.js
@@ -1,5 +1,50 @@
+import homeBackground from './images/homeBackground.jpg';
+
 function generateHomePage() {
-    console.log("HOME");
+    const content = document.getElementById('content');
+
+    const mainContainer = generateElement('div', 'mainContainer', content, undefined);
+    const mainLeft = generateElement('div', 'mainLeft', mainContainer, undefined);
+    const homeMainCaption = generateElement('div', 'homeMainCaption', mainLeft, undefined);
+    generateElement('p', undefined, homeMainCaption, 'Discover your taste');
+    generateElement('h1', undefined, homeMainCaption, 'Eat healthy and Natural Food');
+    const menuButtonMain = generateElement('button', 'menu', homeMainCaption, undefined);
+    generateElement('p', undefined, menuButtonMain, 'Discover Menu');
+
+    const mainRight = generateElement('div', 'mainRight', mainContainer, undefined);
+    const navBar = generateElement('div', 'navbar', mainRight , undefined);
+
+    const navHome = generateElement('div', 'navHome underlineEffect', navBar, undefined);
+    const homeButton = generateElement('button', undefined, navHome, undefined);
+    generateElement('h2', undefined, homeButton, 'Home');
+
+    const navMenu = generateElement('div', 'navMenu underlineEffect', navBar, undefined);
+    const menuButton = generateElement('button', undefined, navMenu, undefined);
+    generateElement('h2', undefined, menuButton, 'Menu');
+
+    const navContact = generateElement('div', 'navContact underlineEffect', navBar, undefined);
+    const contactButton = generateElement('button', undefined, navContact, undefined);
+    generateElement('h2', undefined, contactButton, 'Contact');
+
+    const homeBackgroundImg = new Image();
+    homeBackgroundImg.src = homeBackground;
+    homeBackgroundImg.classList.add('mainRightImg');
+    mainRight.appendChild(homeBackgroundImg);
+}
+
+function generateElement(type, className, parent, innerText) {
+    const element = document.createElement(type);
+    if (className) {
+        const classes = className.split(" ");
+        for (let i = 0; i < classes.length; i++) {
+            element.classList.add(classes[i]);
+        }
+    }
+    if (innerText) {
+        element.innerHTML = innerText;
+    }
+    parent.appendChild(element);
+    return element
 }
 
 export {generateHomePage}
--- src/index.js
+++ src/index.js
@@ -5,29 +5,11 @@ import {generateMenuPage} from './menu.js'
 import {generateContactPage} from './contact.js'
 
 
- function generatePage() {
-    
-    // Test CSS
-    // const element = document.createElement('div');
-    // element.innerHTML = "Testing Styles..."
-    // element.classList.add('hello');
-    
-    // Test assets
-    // const myIcon = new Image();
-    // myIcon.src = Icon;
-    // element.appendChild(myIcon);
-
-    // const mainRightSide = document.querySelector('.mainRight');
-    // const homeBackgroundImg = new Image();
-    // homeBackgroundImg.src = homeBackground;
-    // homeBackgroundImg.classList.add('mainRightImg');
-    // mainRightSide.appendChild(homeBackgroundImg);
+ function loadPage() {
 
     generateHomePage();
-    generateMenuPage();
-    generateContactPage();
-    // return element;
+    // generateMenuPage();
+    // generateContactPage();
 }
 
-// document.body.appendChild(testComponent());
-generatePage();
+loadPage();
