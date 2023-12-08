ff20db68d8340a6e84043e4e119ec093c5b074da
Jeremy <jdavid.rupprecht@gmail.com>
2023-12-07 19:52:51 -0700

    Add webpack asset loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import './style.css';
+import Icon from './testIcon.png';
 
  function testComponent() {
+    
+    // Test CSS
     const element = document.createElement('div');
     element.innerHTML = "Testing Styles..."
     element.classList.add('hello');
+    
+    // Test assets
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
+
     return element;
  }
 
