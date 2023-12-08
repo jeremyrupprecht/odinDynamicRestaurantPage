1cdec388a5e5f448d9fba92df6d3816b92885cce
Jeremy <jdavid.rupprecht@gmail.com>
2023-12-07 19:44:15 -0700

    Add webpack CSS loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("testing....");
+import './style.css';
+
+ function testComponent() {
+    const element = document.createElement('div');
+    element.innerHTML = "Testing Styles..."
+    element.classList.add('hello');
+    return element;
+ }
+
+document.body.appendChild(testComponent());
+console.log("testing...");
