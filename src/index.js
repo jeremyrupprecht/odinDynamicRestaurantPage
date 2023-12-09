0ad9e5b7e2eecd977b55e00be60283893c47acdc
Jeremy <jdavid.rupprecht@gmail.com>
2023-12-08 18:47:30 -0700

    Add media query styling to home page

--- src/index.js
+++ src/index.js
@@ -7,9 +7,15 @@ import {generateContactPage} from './contact.js'
 
  function loadPage() {
 
-    // generateHomePage();
+    generateHomePage();
     // generateMenuPage();
     // generateContactPage();
+
+    // const mainRight = document.querySelector('.mainRight');
+    // const homeBackgroundImg = new Image();
+    // homeBackgroundImg.src = homeBackground;
+    // homeBackgroundImg.classList.add('mainRightImg');
+    // mainRight.appendChild(homeBackgroundImg);
 }
 
 loadPage();
